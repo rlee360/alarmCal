048d23a0361e00524fab40bf19ae3004d91add0a
rlee42 <rl957is@gmail.com>
2018-05-06 21:48:32 -0400

    Added some new functionality, for fun: alarmCal will now create a desktop shortcut

--- alarmCal/install.sh
+++ alarmCal/install.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 chmod a+x calwidget
+cp -r alarmCal_sounds $HOME
+
+cat <<EOT > $HOME/Desktop/alarmCal.desktop
+[Desktop Entry]
+Type=Application
+Name=alarmCal
+Comment=An inclusive alarm/calendar system
+Exec=$PWD/calwidget
+Icon=$PWD/icon.png
+Categories=Office;
+EOT
+
+chmod a+x $HOME/Desktop/alarmCal.desktop
